0fac719ec92b2c31819f6cb77f8439bbc46919c4
carlosb114093 <78057329+carlosb114093@users.noreply.github.com>
2025-03-21 15:54:35 -0500

    AudioManager y GameManager

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -1,16 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+
+    public static AudioManager Instance { get; private set; }
+    [SerializeField] AudioMixer master;
+    [SerializeField] AudioSource sfxAudio, musicAudio;
+    public Sound[] musicSounds, sfxSounds;
+
+    
+
+    void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
         
+        PlayMusic("Menu Theme");
     }
+
+    public void PlaySFX(string name)
+    {
+        Sound sfx = Array.Find(sfxSounds, x => x.nameSound == name);
+
+        if (sfx == null)
+        {
+            Debug.Log("Sound Not Found");
+        }
+
+        else
+        {
+            sfxAudio.PlayOneShot(sfx.clip);
+        }
+    }
+
+    public void PlayMusic(string name)
+    {
+        Sound music = Array.Find(musicSounds, x => x.nameSound == name);
+
+        if (music == null)
+        {
+            Debug.Log("Sound Not Found");
+            Debug.Log(music);
+        }
+
+        else
+        {
+            musicAudio.clip = music.clip;
+            musicAudio.Play();
+            musicAudio.loop = true;
+        }
+    }
+
+    public void RestartMusic()
+{
+    musicAudio.Stop();
+    musicAudio.Play();
+}
+
 }
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,16 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
-{
+{public bool juegoIniciado = false;
+    public int puntaje;
+    public GameObject panelVictoria;
+    public GameObject inicialText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+       //AudioManager.Instance.PlayMusic("Game Theme"); 
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            IniciarJuego();
+            inicialText.SetActive(false);
+        }
         
+    }
+
+    public void IniciarJuego()
+    {
+        juegoIniciado = true;
+        Time.timeScale = 1f;        
+        inicialText.SetActive(true);
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene(1);
+        //AudioManager.Instance.PlayMusic("Game Over Theme");
+    }
+
+    public void MostrarMensajeVictoria()
+    {
+        if (panelVictoria != null)
+        {
+            panelVictoria.SetActive(true);
+        }
+        Debug.Log("¡Has ganado!");
+        Time.timeScale = 0f;
+        juegoIniciado = false;
     }
 }

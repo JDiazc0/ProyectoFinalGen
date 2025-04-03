2ccf494faaf1f1fc370184ba141bd2d63d6e3a4d
Camilo59 <korposoft@gmail.com>
2025-04-03 12:30:44 -0500

    AcertijoSecuencia02

--- Assets/Scripts/UI/ObjectMessage.cs
+++ Assets/Scripts/UI/ObjectMessage.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class ObjectMessage : MonoBehaviour
 {
     public GameObject canvasMensaje;
+    public GameObject activatePortal;
 
     void OnTriggerEnter(Collider other)
     {
@@ -10,6 +11,10 @@ public class ObjectMessage : MonoBehaviour
         {
             Debug.Log("El objeto " + other.gameObject.name + " entró en el trigger.");
             canvasMensaje.SetActive(true);
+        }
+        if (other.CompareTag("Mug"))
+        {
+            activatePortal.SetActive(true);
         }
         
     }

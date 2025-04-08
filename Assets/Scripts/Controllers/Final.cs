73af4444e98d3a1706f6e6c8e3910affdd4f0adb
carlosb114093 <78057329+carlosb114093@users.noreply.github.com>
2025-04-08 04:23:34 -0500

    imagenes finales alternativos

---
+++ Assets/Scripts/Controllers/Final.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using TMPro;
+
+public class MutantRatTrigger : MonoBehaviour
+{
+    public TextMeshProUGUI mensajeTexto;
+    public string mensaje = "Has encontrado la rata con el gen mutante. Esta será prueba suficiente para revelar los secretos del doctor.";
+    public float tiempoMensaje = 7f;
+
+    private void Start()
+    {
+        if (mensajeTexto != null)
+            mensajeTexto.gameObject.SetActive(false); // Oculta el mensaje al inicio
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Terapia")) return; // Solo responde si el objeto tiene el tag "Terapia"
+
+        // Mostrar mensaje
+        if (mensajeTexto != null)
+        {
+            mensajeTexto.text = mensaje;
+            mensajeTexto.gameObject.SetActive(true);
+            Invoke(nameof(OcultarMensaje), tiempoMensaje);
+        }
+
+        // Cambiar la etiqueta del objeto colisionado
+        other.tag = "Exit";
+        Debug.Log($"La etiqueta de {other.name} ahora es: {other.tag}");
+    }
+
+    private void OcultarMensaje()
+    {
+        if (mensajeTexto != null)
+            mensajeTexto.gameObject.SetActive(false);
+    }
+}
